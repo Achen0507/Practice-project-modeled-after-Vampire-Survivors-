d368657a83b048a155792973e75ade3c8eab4934
youshinkin <youshinkin@gmail.com>
2026-05-25 14:43:19 +0800

    删除多余using

--- Assets/Scripts/Passives/PassiveInstance.cs
+++ Assets/Scripts/Passives/PassiveInstance.cs
@@ -1,13 +1,11 @@
 using Survivor.Data;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace Survivor.Passives
 {
     /// <summary>
     /// 被动技能实例（玩家拥有）
     /// </summary>
+    
     public class PassiveInstance
     {
         public PassiveData data;
