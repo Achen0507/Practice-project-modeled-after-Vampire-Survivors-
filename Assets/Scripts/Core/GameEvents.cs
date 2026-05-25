6a12cb2c1b1baffdcd91734bfa90124415ca72c8
youshinkin <youshinkin@gmail.com>
2026-05-25 16:01:29 +0800

    分支

--- Assets/Scripts/Core/GameEvents.cs
+++ Assets/Scripts/Core/GameEvents.cs
@@ -87,5 +87,7 @@ namespace Survivor.Core
         /// <summary> 局外数值改变 </summary>
         public static event Action OnPlayerStatsChanged;
         public static void PlayerStatsChanged() => OnPlayerStatsChanged?.Invoke();
+
+        //在这里增加一行注释
     }
 }
